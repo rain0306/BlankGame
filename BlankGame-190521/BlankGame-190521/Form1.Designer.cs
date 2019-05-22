1a3c3947479ccd0e4647a3e8977fabefa6f17b9a
rain_sun <tatu19990306@gmail.com>
2019-05-22 16:20:53 +0900

    初期

---
+++ BlankGame-190521/BlankGame-190521/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BlankGame_190521
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.count = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // count
+            // 
+            this.count.AutoSize = true;
+            this.count.Location = new System.Drawing.Point(380, 201);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(11, 12);
+            this.count.TabIndex = 0;
+            this.count.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.count);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label count;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BlankGame-190521/BlankGame-190521/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlankGame_190521
+{
+    public partial class Form1 : Form
+    {
+        private int cont = 0;
+        Task task;
+        Task task2;
+        public Form1()
+        {
+            InitializeComponent();
+            Thread.Sleep(1000);
+            //loop();
+        }
+
+        private void loop()
+        {
+            if (this.count.InvokeRequired)
+            {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        count.Text = cont++.ToString();
+                    });
+            }
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (task == null)
+            {
+                task = Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        loop();
+                    }
+
+                });
+            }
+
+        }
+    }
+
+}
